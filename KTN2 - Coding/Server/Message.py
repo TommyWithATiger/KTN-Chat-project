bc38517b765f7c443ab051be10ebfc3bc425a3fc
TommyWithATiger <halvard.hummel@hotmail.com>
2016-03-01 14:17:07 +0100

    Small syntax change

--- KTN2 - Coding/Server/Message.py
+++ KTN2 - Coding/Server/Message.py
@@ -4,13 +4,13 @@ import json
 class Message:
     def __init__(self, text, user, timestamp):
         self.message_text = text
-        self.user = user
+        self.username = user
         self.timestamp = timestamp
 
     def to_JSON(self):
         return json.dumps({
             'timestamp': self.timestamp,
-            'sender': self.user,
+            'sender': self.username,
             'response': "message",
             'content': self.message_text
         })
