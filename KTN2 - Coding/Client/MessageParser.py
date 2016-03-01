174dca71a116acad437c1971dfc647257e72d037
ubuu <henrik-olsvik@outlook.com>
2016-03-01 10:16:03 +0100

    MessageParser.py Attempt
    First attempt at doing MessageParser

--- KTN2 - Coding/Client/MessageParser.py
+++ KTN2 - Coding/Client/MessageParser.py
@@ -1,4 +1,10 @@
+import json
+import datetime
 
+def print_formatted_message(timestamp, response_type, content):
+    print(timestamp, response_type, content)
+
+#Pointless comment.
 
 class MessageParser():
     def __init__(self):
@@ -6,19 +12,32 @@ class MessageParser():
         self.possible_responses = {
             'error': self.parse_error,
             'info': self.parse_info,
-	    # More key:values pairs are needed	
+	    'messsage': self.parse_message,
+            'history': self.parse_history
         }
 
     def parse(self, payload):
-        payload = # decode the JSON object
+        payload = json.loads(payload)
 
         if payload['response'] in self.possible_responses:
             return self.possible_responses[payload['response']](payload)
         else:
-            # Response not valid
+            print_formated_message(payload['timestamp'], "error", "Recieved impossible response")
 
     def parse_error(self, payload):
-    
+        payload = json.loads(payload)
+        print_formated_message(payload['timestamp'], payload['response'], payload['content'])
+        
     def parse_info(self, payload):
-    
-    # Include more methods for handling the different responses... 
+        payload = json.loads(payload)
+        print_formated_message(payload['timestamp'], payload['response'], payload['content'])
+        
+    def parse_message(payload):
+        payload = json.loads(payload)
+        print_formated_message(payload['timestamp'], payload['response'], payload['content'])
+
+    def parse_history(payload):
+        payload = json.loads(payload)
+        for i in payload.items():
+            parse_formatted_message(payload[i]['timestamp'], payload[i]['repsonse'], payload[i]['content'])
+
