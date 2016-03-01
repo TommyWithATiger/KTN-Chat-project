10867ba55111991c58bcb650f4ede5a6326795c4
TommyWithATiger <halvard.hummel@hotmail.com>
2016-03-01 11:44:32 +0100

    Syntax cleanup

--- KTN2 - Coding/Client/MessageParser.py
+++ KTN2 - Coding/Client/MessageParser.py
@@ -1,10 +1,11 @@
 import json
-import datetime
+
 
 def print_formatted_message(timestamp, response_type, content):
     print(timestamp, response_type, content)
 
-#Pointless comment.
+
+# Pointless comment.
 
 class MessageParser():
     def __init__(self):
@@ -12,7 +13,7 @@ class MessageParser():
         self.possible_responses = {
             'error': self.parse_error,
             'info': self.parse_info,
-	    'messsage': self.parse_message,
+            'message': self.parse_message,
             'history': self.parse_history
         }
 
@@ -22,22 +23,21 @@ class MessageParser():
         if payload['response'] in self.possible_responses:
             return self.possible_responses[payload['response']](payload)
         else:
-            print_formated_message(payload['timestamp'], "error", "Recieved impossible response")
+            print_formatted_message(payload['timestamp'], "error", "Recieved impossible response")
 
     def parse_error(self, payload):
         payload = json.loads(payload)
-        print_formated_message(payload['timestamp'], payload['response'], payload['content'])
-        
+        print_formatted_message(payload['timestamp'], payload['response'], payload['content'])
+
     def parse_info(self, payload):
         payload = json.loads(payload)
-        print_formated_message(payload['timestamp'], payload['response'], payload['content'])
-        
+        print_formatted_message(payload['timestamp'], payload['response'], payload['content'])
+
     def parse_message(payload):
         payload = json.loads(payload)
-        print_formated_message(payload['timestamp'], payload['response'], payload['content'])
+        print_formatted_message(payload['timestamp'], payload['response'], payload['content'])
 
     def parse_history(payload):
-        payload = json.loads(payload)
+        payload = payload['history']
         for i in payload.items():
-            parse_formatted_message(payload[i]['timestamp'], payload[i]['repsonse'], payload[i]['content'])
-
+            print_formatted_message(payload[i]['timestamp'], payload[i]['repsonse'], payload[i]['content'])
