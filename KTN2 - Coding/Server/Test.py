4cda14babd97e538441875e20a002a235160d9b1
TommyWithATiger <halvard.hummel@hotmail.com>
2016-03-02 15:22:01 +0100

    Updated server test to check with multiple users for login/logout and messages. This way the server also has the ability to build up a history without running the test several times

--- KTN2 - Coding/Server/Test.py
+++ KTN2 - Coding/Server/Test.py
@@ -1,55 +1,96 @@
 import socket
 import json
+from time import sleep
+
+clients = []
+client_names = ["client0", "client1", "client2", "client3", "client4", "client5", "client6", "client7", "client8", "client9", "testUser"]
 
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("localhost", 9998))
-client.settimeout(1)
+class Client:
+    def __init__(self, name):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect(('localhost', 9998))
+        self.client.settimeout(1)
+        self.name = name
 
+    def get_name(self):
+        return self.name
 
-def send(request, content):
-    client.send(json.dumps({
-        'request': request,
-        'content': content
-    }).encode("UTF-8"))
+    def send(self, request, content):
+        self.client.send(json.dumps({
+            'request': request,
+            'content': content
+        }).encode("UTF-8"))
 
+    def print_recieved(self):
+        try:
+            received = json.loads(self.client.recv(4096).decode())
+            print(received['timestamp'])
+            print(received['sender'])
+            print(received['response'])
+            print(received['content'] + "\n")
+        except TimeoutError:
+            print("Server did not return anything")
 
-def print_recieved():
-    received = json.loads(client.recv(4096).decode())
-    print(received['timestamp'])
-    print(received['sender'])
-    print(received['response'])
-    print(received['content'] + "\n")
+    def print_recieved_history(self):
+        try:
+            received = json.loads(self.client.recv(4096).decode())
+            print(received['timestamp'])
+            print(received['sender'])
+            print(received['response'])
+            print(received['content'])
+            for message in received['content']:
+                print(message['timestamp'], message['sender'], message['content'])
+            print("\n")
+        except TimeoutError:
+            print("Server did not return anything")
 
+    def get_recieved_content(self):
+        try:
+            return json.loads(self.client.recv(4096).decode())['content']
+        except TimeoutError:
+            return "Server did not return anything"
 
-def print_recieved2():
-    received = json.loads(client.recv(4096).decode())
-    print(received['timestamp'])
-    print(received['sender'])
-    print(received['response'])
-    print(received['content'])
-    for message in received['content']:
-        print(message['timestamp'], message['sender'], message['content'])
-    print("\n")
+for i in range(10):
+    client = Client("client" + str(i))
+    client.send('login', client.name)
+    if client.get_recieved_content() == 'Login successful!':
+        print("Client" + str(i) + " logged in successful")
+    else:
+        print("Client" + str(i) + " did not log in successful")
+    clients.append(client)
+    client.send('msg', 'test message ' + str(i + 1))
 
-send("help", "")
-print_recieved()
+client = Client("testUser")
+client.send("help", "")
+client.print_recieved()
+sleep(0.05)
+client.send("msg", "Test message")
+client.print_recieved()
+sleep(0.05)
+client.send("login", "")
+client.print_recieved()
+sleep(0.05)
+client.send("login", "testUser")
+client.print_recieved()
+client.print_recieved_history()
+sleep(0.05)
+client.send("msg", "test")
+sleep(0.05)
+client.send("names", "")
+client.print_recieved()
+sleep(0.05)
 
-send("msg", "test")
-print_recieved()
+for client2 in clients:
+    client2.send("logout", "")
+    client_names = client_names[1:]
+    client.send("names", "")
+    for client_name in client.get_recieved_content().split(", "):
+        if client_name not in client_names:
+            print(client2.get_name() + " did not logout successful")
+            break
+    else:
+        print(client2.get_name() + " logged out successful")
+    sleep(0.05)
 
-send("login", "fssdaaes")
-print_recieved()
-try:
-    print_recieved2()
-except:
-    pass
-
-
-send("msg", "test")
-
-
-send("names", "")
-print_recieved()
-
-send("logout", "")
+client.send("logout", "")
