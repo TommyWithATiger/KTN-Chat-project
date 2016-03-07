08b0164294a45a3d18fa1031d094b2955943b838
RovelMan <jotdeepgahla@hotmail.com>
2016-03-07 12:48:11 +0100

    client filen
    legger ut midlertidig

--- KTN2 - Coding/Client/Client.py
+++ KTN2 - Coding/Client/Client.py
@@ -3,37 +3,31 @@ import socket
 from MessageReceiver import MessageReceiver
 from MessageParser import MessageParser
 
-
 class Client:
-    """
-    This is the chat client class
-    """
+    """This is the chat client class"""
 
     def __init__(self, host, server_port):
-        """
-        This method is run when creating a new Client object
-        """
-
-        # Set up the socket connection to the server
+        """This method is run when creating a new Client object"""
+        self.host = host
+        self.server_port = server_port
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
-        # TODO: Finish init process with necessary code
         self.run()
 
     def run(self):
-        # Initiate the connection to the server
+        self.messagereceiver = MessageReceiver(self, self.connection)
         self.connection.connect((self.host, self.server_port))
         
     def disconnect(self):
-        # TODO: Handle disconnection
+        self.messagereceiver.MessageReceiver(self, self.connection)
+        self.connection.exit()
         pass
 
     def send_payload(self, data):
-        # TODO: Handle sending of a payload
+        self.data = data
+        self.connection.send(self.data.encode("UTF-8"))
         pass
         
     # More methods may be needed!
-
 
 if __name__ == '__main__':
     """
