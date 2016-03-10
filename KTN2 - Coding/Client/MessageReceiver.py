c52abcc42d6ee0d8855beda0274eb1dbe993fc69
ComradePetah <petah92@gmail.com>
2016-03-10 08:32:47 +0100

    Update MessageReceiver.py
    Think this should work.

--- KTN2 - Coding/Client/MessageReceiver.py
+++ KTN2 - Coding/Client/MessageReceiver.py
@@ -8,11 +8,15 @@ class MessageReceiver(Thread):
     is necessary to make the MessageReceiver start a new thread, and it allows
     the chat client to both send and receive messages at the same time
     """
+    
 
     def __init__(self, client, connection):
         """
         This method is executed when creating a new MessageReceiver object
         """
+        threading.Thread.__init__(self)
+        jsonSocket.JsonServer.__init__(self)
+        self._isAlive = False
 
         # Flag to run thread as a deamon
         self.daemon = True
@@ -21,4 +25,26 @@ class MessageReceiver(Thread):
 
     def run(self):
         # TODO: Make MessageReceiver receive and handle payloads
+        while self._isAlive:
+            try:
+                self.acceptConnection()
+            except socket.timeout as e:
+                logger.debug("socket.timeout: %s" % e)
+                continue
+            except Exception as e:
+                logger.exception(e)
+                continue
+ 
+            while self._isAlive:
+                try:
+                    obj = self.readObj()
+                    self._processMessage(obj)
+                except socket.timeout as e:
+                    logger.debug("socket.timeout: %s" % e)
+                    continue
+                except Exception as e:
+                    logger.exception(e)
+                    self._closeConnection()
+                    break
+
         pass
